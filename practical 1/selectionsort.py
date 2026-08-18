fa46d8acb3b9b13c6ac53cc0ceb31053181fa44c
Bommana Deepak <bommanadeepak68@gmail.com>
2026-08-18 21:33:35 +0530

    Delete practical 1 directory

--- practical 1/selectionsort.py
+++
@@ -1,9 +0,0 @@
-arr = [3,7,19,20,-4]
-n = len(arr)
-for i in range(n-1):
-    min =i
-    for j in range(i+1,n):
-        if arr[j]< arr[min]:
-            min = j
-        arr[i],arr[min]=arr[min],arr[i]
-print(arr)
